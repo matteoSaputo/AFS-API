98c1c29b59894f2d069c2451b1ad17dd341e794c
matteoSaputo <matteosaputo1201@gmail.com>
2026-04-08 12:09:30 -0400

    Fix: Prod running. Typo in prod url. default endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
+import { fail } from "node:assert";
 import { businessRouter } from "./routes/businesses";
 import { debugRouter } from "./routes/debug";
 import { healthRouter } from "./routes/health";
+import { industryRouter } from "./routes/industries";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 		const pathname = url.pathname;
 
+		if(pathname === "/"){
+			return Response.json({
+				ok: true,
+				service: "AFS API",
+				status: "running"
+			})
+		}
+
 		// ======== Businesses ==========
 		if (pathname.startsWith("/businesses")) {
 			return businessRouter(request, env);
+		}
+
+		// ======== Industries ==========
+		if(pathname.startsWith("/industries")) {
+			return industryRouter(request, env)
 		}
 
 		// ======== DB Health ==========
@@ -22,8 +37,7 @@ export default {
 			return debugRouter(request, env);
 		}
 
-		return new Response("Not Found", {
-			status: 404
-		});
+		// return fail(`Endpoint Not Found, URL: ${url}`);
+		return fail("Endpoint Not Found");
 	},
 } satisfies ExportedHandler<Env>;
